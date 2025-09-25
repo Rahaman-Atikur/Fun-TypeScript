559cb561407c8496c19f79bdc3aee8b8283bbad1
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-09-25 10:34:03 +0100

    First Commit

---
+++ output/script.js
@@ -0,0 +1,6 @@
+console.log("hello world");
+console.log("That is Fine");
+console.log("Yes Run");
+console.log("Don't Run");
+export {};
+//# sourceMappingURL=script.js.map
---
+++ src/script.ts
@@ -0,0 +1,5 @@
+console.log("hello world");
+console.log("That is Fine");
+
+console.log("Yes Run");
+console.log("Don't Run");
