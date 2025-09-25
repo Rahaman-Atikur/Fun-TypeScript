2341a2bf7e4effb2fc4c3d637a16d4de7fb87fff
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-09-25 12:04:23 +0100

    Type is Familiar Now

--- output/script.js
+++ output/script.js
@@ -1,6 +1,21 @@
-const country = "I love bangladesh";
-console.log(country);
-const name = "atikur";
-console.log(name);
+// let fruits = ['Banana','Orange','Apple'];
+// fruits.push(23);
+// let a: string ;
+// let b: number;
+// a="Atikur";
+// console.log(a);
+let c;
+c = {
+    name: 'Atikur',
+    age: '34',
+    isHuman: true,
+};
+console.log(c);
+let d = {
+    name: "atikur",
+    age: 34,
+    isHuman: false,
+};
+console.log(d);
 export {};
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,26 @@
-const country = "I love bangladesh";
-console.log(country);
+// let fruits = ['Banana','Orange','Apple'];
+// fruits.push(23);
+// let a: string ;
+// let b: number;
 
-const name = "atikur";
-console.log(name);
+// a="Atikur";
+// console.log(a);
+let c: object;
+c = {
+    name: 'Atikur',
+    age: '34',
+    isHuman: true,
+}
+console.log(c);
+
+let d: {
+    name: string,
+    age: number,
+    isHuman: boolean,
+}=
+{
+    name:"atikur",
+    age:34,
+    isHuman:false,
+}
+console.log(d);
